88a30d724402f463571796cbf4fae00efbd98da8
Th-Henrii <thiaghenrique145@gmail.com>
2025-02-03 22:39:09 -0300

    #2 Created entites: Task

---
+++ to-do-list/src/main/java/com/example/to_do_list/entities/Task.java
@@ -0,0 +1,63 @@
+package com.example.to_do_list.entities;
+
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "tb_task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskId;
+
+    @Column(columnDefinition = "TEXT")
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private boolean status;
+
+    public Task(){
+
+    }
+
+    public Task(Long taskId, String title, String description, boolean status) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
